fabbd872844ec08f47e2c3cbd9f0e19c4f80ac03
Alex Simonian <alex.simonian@gmail.com>
2017-04-23 16:26:33 -0500

    Add comments

--- App.elm
+++ App.elm
@@ -9,20 +9,25 @@ import Html exposing (Html, div, text, program)
 type alias Model =
     String
 
-
+-- provides the initial input for the application
+-- Html.program expects a tuple with (model, command)
 init : ( Model, Cmd Msg )
 init =
     ( "Hello", Cmd.none )
 
 
 -- MESSAGES
-
+-- are things that happen in our applications that our component
+-- responds to.  In this case, the application doesn't do anything,
+-- so we only have a message called NoOp
 
 type Msg
     = NoOp
 
 
 -- VIEW
+-- The functioon view renders an Html element using our application model.
+-- This means that this Html element would produce messages tagged with Msg.
 
 view : Model -> Html Msg
 view model =
@@ -31,6 +36,11 @@ view model =
 
 
 -- UPDATE
+-- This function will be called by Html.program each time a
+-- message is received.  This update function responds to messages
+-- updating the model and returning commands as needed.
+-- In this example, we only respond to NoOp and return the unchanged
+-- model and Cmd.none (meaning no command to perform)
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -39,6 +49,9 @@ update msg model =
             ( model, Cmd.none )
 
 -- SUBCRIPTIONS
+-- We use subscriptions to listen for external input to our application.
+-- Some examples of subscriptions: mouse movement, keyboard events, browser changes
+-- In this case, we are not interested in any external input so we use Sub.none
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -46,7 +59,8 @@ subscriptions model =
 
 
 -- MAIN
-
+-- Wires everything together and returns and html element that we can
+-- render in the page. program takes our init, view, update and subscriptions.
 
 main : Program Never Model Msg
 main =
